187db577f8dc93c496741a686e05ecdc3724b3e2
jchatkinson <jchatkinson@gmail.com>
2019-09-01 08:33:43 -0700

    init commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { DynamicFormModel, DynamicFormService } from '@ng-dynamic-forms/core';
+import { MY_FORM_MODEL } from './dynamic-form.model';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,49 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'dynamic-forms';
+  calc = {
+    title: "My Calculation Template",
+    subtitle: "My calculation subtitle",
+    input: [{
+      "type":"INPUT",
+      "inputType":"number",
+      "id":"fc",
+      "label": "Concrete Strength",
+      "prefix": "f<sub>c</sub> = ",
+      "suffix": "ksi",
+      "value": 5.0,
+      "min": 0,
+      "max":20
+    },
+    {
+      "type":"INPUT",
+      "inputType":"number",
+      "id":"bw",
+      "label": "Width",
+      "prefix": "b<sub>w</sub> = ",
+      "suffix": "in",
+      "value": 24,
+      "min": 0
+    },
+    {
+      "type":"INPUT",
+      "inputType":"number",
+      "id":"h",
+      "label": "depth",
+      "prefix": "h = ",
+      "suffix": "in",
+      "value": 36,
+      "min": 0,
+    }],
+    output: '<p>This is my html output</p><p>It can be multiline</p>',
+    notes: []
+  };
+  formModel: DynamicFormModel = this.formService.fromJSON(this.calc.input);
+  formGroup: FormGroup;
+
+  constructor(private formService: DynamicFormService) {}
+
+  ngOnInit() {
+    this.formGroup = this.formService.createFormGroup(this.formModel);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { ReactiveFormsModule, FormsModule } from '@angular/forms'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DynamicFormsCoreModule } from '@ng-dynamic-forms/core';
+import { DynamicFormsMaterialUIModule } from '@ng-dynamic-forms/ui-material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCardModule, MatInputModule, MatButtonModule, MatCheckboxModule, MatFormFieldModule, MatSliderModule, MatTooltipModule } from '@angular/material'
 
 @NgModule({
   declarations: [
@@ -10,7 +14,19 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    DynamicFormsCoreModule,
+    DynamicFormsMaterialUIModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatInputModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatFormFieldModule,
+    MatSliderModule,
+    MatTooltipModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
